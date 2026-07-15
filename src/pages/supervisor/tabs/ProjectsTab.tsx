28d002903a2fd41cd535e3f4d69ec7dba4143155
Satheesh Dharmaraj <satheesh.drj@gmail.com>
2026-07-15 09:44:20 +0400

    Tighten New Project modal spacing and sizing

--- src/pages/supervisor/tabs/ProjectsTab.tsx
+++ src/pages/supervisor/tabs/ProjectsTab.tsx
@@ -1201,6 +1201,7 @@ export default function ProjectsTab({ onOpenTechnicianWorkflow }: ProjectsTabPro
         <Modal
           title="New Project"
           size="wide"
+          bodyClassName="pj-create-project-modal-body"
           onClose={closeCreateModal}
           closeOnBackdrop={false}
           closeOnEscape={!saving}
